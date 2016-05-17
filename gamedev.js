a556bf095145880dc90fdb8c25b272986d0a31ac
Waffles-McPancakes <200099@hkis.edu.hk>
2016-05-17 10:31:23 +0800

    fix

--- gamedev.js
+++ gamedev.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 $(document).bind('mousemove', function(e){
     $('#follower').css({
        left:  e.pageX - 30,
-       top:   e.pageY + 10
+       top:   e.pageY
     });
 });
 
