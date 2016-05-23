f158820ebd14ce9d688c2b8a236661afe3887428
xtremeskater900 <200411@hkis.edu.hk>
2016-05-23 09:55:30 +0800

    Revert "WASD maybe"
    This reverts commit 84e2f5c4a9edf967731450a615438d2eadddcc2a.

--- gamedev.js
+++ gamedev.js
@@ -1,51 +1,16 @@
 
 $( document ).ready(function() {
 
-var keyW;
-var keyA;
-var keyS;
-var keyD;
 
-function onKeyDown(event) {
-  var keyCode = event.keyCode;
-  switch (keyCode) {
-    case 68:
-      keyD = true;
-      break;
-    case 83:
-      keyS = true;
-      break;
-    case 65:
-      keyA = true;
-      break;
-    case 87:
-      keyW = true;
-      break;
-    default:
-      keyW = false;
-      keyA = false;
-      keyS = false;
-      keyD = false;
-  }
-}
 
-function onKeyUp(event) {
-  var keyCode = event.keyCode;
-  switch (keyCode) {
-    case 68:
-      keyD = false;
-      break;
-    case 83:
-      keyS = false;
-      break;
-    case 65:
-      keyA = false;
-      break;
-    case 87:
-      keyW = false;
-      break;
-  }
-}
+
+
+$(document).bind('mousemove', function(e){
+    $('#follower').css({
+       left:  e.pageX - 30,
+       top:   e.pageY
+    });
+});
 
 
 
