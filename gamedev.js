a213a33ea8754ef3d762ecd2040e64bf2b56c671
Waffles-McPancakes <200099@hkis.edu.hk>
2016-05-23 09:40:48 +0800

    REVERT THIs

--- gamedev.js
+++ gamedev.js
@@ -3,14 +3,26 @@ $( document ).ready(function() {
 
 
 
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        break;
 
+        case 38: // up
+        break;
 
-$(document).bind('mousemove', function(e){
-    $('#follower').css({
-       left:  e.pageX - 30,
-       top:   e.pageY
-    });
+        case 39: // right
+        break;
+
+        case 40: // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
+
+
 
 
 
