5f2c927f5ab041be3165a1e31123b7fb8b4d910b
Waffles-McPancakes <200099@hkis.edu.hk>
2016-05-23 10:07:38 +0800

    edited moving

--- gamedev.js
+++ gamedev.js
@@ -9,22 +9,22 @@ $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
         $("#follower").stop().animate({
-            left: '-=100'
+            left: '-=50'
         }); //left arrow key
         break;
     case 38:
         $("#follower").stop().animate({
-            top: '-=100'
+            top: '-=50'
         }); //up arrow key
         break;
     case 39:
         $("#follower").stop().animate({
-            left: '+=100'
+            left: '+=50'
         }); //right arrow key
         break;
     case 40:
         $("#follower").stop().animate({
-            top: '+=100'
+            top: '+=50'
         }); //bottom arrow key
         break;
     }
