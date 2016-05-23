1a6422618496133a1935033bb1b484641cd3677c
xtremeskater900 <200411@hkis.edu.hk>
2016-05-23 09:29:09 +0800

    try to drag

--- gamedev.js
+++ gamedev.js
@@ -2,17 +2,10 @@
 $( document ).ready(function() {
 
 
-
-
-
-$(document).bind('mousemove', function(e){
-    $('#follower').css({
-       left:  e.pageX - 30,
-       top:   e.pageY
-    });
-});
-
-
+  
+  $(function() {
+    $( "#follower" ).draggable();
+  });
 
 
 });
