59eb927d64e83fe4a5f141269f5e707ad2f2d30b
Waffles-McPancakes <200099@hkis.edu.hk>
2016-05-23 10:06:27 +0800

    Arrow Keys maybe

--- gamedev.js
+++ gamedev.js
@@ -5,13 +5,30 @@ $( document ).ready(function() {
 
 
 
-$(document).bind('mousemove', function(e){
-    $('#follower').css({
-       left:  e.pageX - 30,
-       top:   e.pageY
-    });
-});
-
+$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $("#follower").stop().animate({
+            left: '-=100'
+        }); //left arrow key
+        break;
+    case 38:
+        $("#follower").stop().animate({
+            top: '-=100'
+        }); //up arrow key
+        break;
+    case 39:
+        $("#follower").stop().animate({
+            left: '+=100'
+        }); //right arrow key
+        break;
+    case 40:
+        $("#follower").stop().animate({
+            top: '+=100'
+        }); //bottom arrow key
+        break;
+    }
+})
 
 
 
