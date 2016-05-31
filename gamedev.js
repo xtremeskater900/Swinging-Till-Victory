2a80b1e39fd525d6ee4f1ed076a2c9b59592fca2
Waffles-McPancakes <200099@hkis.edu.hk>
2016-05-31 09:45:47 +0800

    added collision

--- gamedev.js
+++ gamedev.js
@@ -4,6 +4,11 @@ $( document ).ready(function() {
 
 
 
+  $("#gameend").hide();
+  //tell JavaScript what div is the sprite - use the div id
+  var myCar = $("#myCar");
+  var score = 4
+
 
 $(document).keydown(function(e) {
     switch (e.which) {
@@ -30,7 +35,109 @@ $(document).keydown(function(e) {
     }
 })
 
+  //this is where the JavaScript collision starts. First we define all the variables of how the collision will happen
+  function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
 
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('.otherCar'), function() {
+      if (collision($('#myCar'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+        $('#result').text('True');
+        //if #myCar hits .othercar, then #results will say "TRUE"
+        score = score - 1;
+        
+        if (score = 3) {
+          $("#health3").hide();
+          
+          window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('.otherCar'), function() {
+      if (collision($('#myCar'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+        $('#result').text('True');
+        //if #myCar hits .othercar, then #results will say "TRUE"
+        score = score - 1;
+        
+        if (score = 2) {
+          $("#health2").hide();
+          
+          window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('.otherCar'), function() {
+      if (collision($('#myCar'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+        $('#result').text('True');
+        //if #myCar hits .othercar, then #results will say "TRUE"
+          score = score - 1;
+        
+         if (score = 1) {
+          $("#health1").hide();
+          
+          
+          
+          
+        }
+        
+      
+          
+          
+
+        //all the actions that happen during a collision go here
+        
+
+      }
+    });
+  }, 200);
+          
+          
+          
+          
+          
+          
+        }
+          
+          
+
+        //all the actions that happen during a collision go here
+        
+
+      }
+    });
+  }, 200);
+          
+        
+        }
+          
+          
+
+        //all the actions that happen during a collision go here
+        
+
+      }
+    });
+  }, 200); //this is how often it checks for a collision
+  
+  
 
 });
 
